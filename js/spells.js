cc422a1930d75fe20b809dd548fa6972cfad7e30
jacobhackett <jrhackett@wpi.edu>
2016-06-17 21:17:26 -0400

    implementing debounce on search to try to optimize performance

--- js/spells.js
+++ js/spells.js
@@ -1,5 +1,7 @@
 $jq = jQuery.noConflict();
 
+$jq('#spell-search').keyup(debounce(handleSearch, 75));
+
 // need to improve performance - look into jquery debounce for this
 function handleSearch() {
     var query = $jq("#spell-search").val().toLowerCase();
@@ -11,5 +13,22 @@ function handleSearch() {
         ($name.text().toLowerCase().indexOf(query) !== -1) || ($school.text().toLowerCase().indexOf(query) !== -1) || ($level.text().toLowerCase().indexOf(query) !== -1) ? $toChange.show() : $toChange.hide();
     });
 }
+
+//taken from underscore.js
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        console.log(timeout);
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 
 
