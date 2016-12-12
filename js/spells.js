cf03c83c4760acbd1fa6c8d89945772afe2f3ed3
Jacob Hackett <jacobrhackett@gmail.com>
2016-12-12 13:45:55 -0500

    adding sorting functionality for classes, schools, and levels. moving search to just the name of spells.

--- js/spells.js
+++ js/spells.js
@@ -1,34 +1,60 @@
-$jq = jQuery.noConflict();
 
-$jq('#spell-search').keyup(debounce(handleSearch, 75));
 
-// need to improve performance - look into jquery debounce for this
-function handleSearch() {
-    var query = $jq("#spell-search").val().toLowerCase();
-    $jq.each($jq(".spell"), function(key, $val) {
-        $toChange = $jq("#" + $val.id);
-        $name = $jq("#" + $val.id + " .spell-name");
-        $level = $jq("#" + $val.id + " .spell-level");
-        $school = $jq("#" + $val.id + " .spell-school");
-        ($name.text().toLowerCase().indexOf(query) !== -1) || ($school.text().toLowerCase().indexOf(query) !== -1) || ($level.text().toLowerCase().indexOf(query) !== -1) ? $toChange.show() : $toChange.hide();
+(function($) {
+
+    $('#spell-search').keyup(debounce(handleSort, 75, true));
+
+    $('select[name="class-select"]').on('change', function() {
+        handleSort();
     });
-}
 
-//taken from underscore.js
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
+    $('select[name="school-select"]').on('change', function() {
+        handleSort();
+    });
+
+    $('select[name="level-select"]').on('change', function() {
+        handleSort();
+    });
+
+    function handleSort() {
+        $class_select = $('select[name="class-select"]').val().toLowerCase();
+        $level_select = $('select[name="level-select"]').val().toLowerCase().substring(6);
+        $school_select = $('select[name="school-select"]').val().toLowerCase();
+        var query = $("#spell-search").val().toLowerCase();
+
+        $.each($(".spell"), function(key, $val) { 
+            $toChange = $("#" + $val.id);
+            $name = $("#" + $val.id + " .spell-name");
+            $level = $("#" + $val.id + " .spell-level");
+            $school = $("#" + $val.id + " .spell-school");
+            $class = $("#" + $val.id + " .spell-classes");
+
+            ($name.text().toLowerCase().indexOf(query) !== -1) 
+                && (($school.text().toLowerCase().indexOf($school_select) !== -1) || $school_select.indexOf("any") !== -1)
+                && (($level.text().toLowerCase().indexOf($level_select) !== -1) || $level_select.indexOf("any") !== -1)
+                && (($class.text().toLowerCase().indexOf($class_select) !== -1) || $class_select.indexOf("any") !== -1)
+                    ? $toChange.show() : $toChange.hide();
+        });
+    }
+
+    //taken from underscore.js
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            console.log(timeout);
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
-        console.log(timeout);
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
     };
-};
+
+}) (jQuery);
+
 
 
